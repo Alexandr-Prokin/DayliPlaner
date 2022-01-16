632232463494620ea6af5d97045e165fc0dc2777
Alexandr Prokin <aaprokin90@yandex.ru>
2022-01-16 20:02:12 +0300

    add dataBinding

--- app/src/main/java/com/example/dayliplaner_v1/presentation/casedescription/CaseDescriptionViewModel.kt
+++ app/src/main/java/com/example/dayliplaner_v1/presentation/casedescription/CaseDescriptionViewModel.kt
@@ -2,4 +2,9 @@ package com.example.dayliplaner_v1.presentation.casedescription
 
 import androidx.lifecycle.ViewModel
 
-class CaseDescriptionViewModel : ViewModel()
+class CaseDescriptionViewModel : ViewModel(){
+    var name : String? = null
+    var description : String? = null
+    var time : String? = null
+}
+
