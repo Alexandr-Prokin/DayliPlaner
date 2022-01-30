088429fa1aad60b15ba8731c8ebdeb296259f576
Alexandr Prokin <aaprokin90@yandex.ru>
2022-01-30 19:00:59 +0300

    add Unit test - GetTimeUseCaseTest

---
+++ app/src/test/java/com/example/dayliplaner_v1/domain/usecase/GetTimeUseCaseTest.kt
@@ -0,0 +1,16 @@
+package com.example.dayliplaner_v1.domain.usecase
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GetTimeUseCaseTest {
+
+    @Test
+    fun getTime() {
+        val testTimeStamp = "1643544000"
+        val useCase = GetTimeUseCase()
+        val actual = useCase.getTime(testTimeStamp)
+        val expected = "15:00"
+        assertEquals(expected, actual)
+    }
+}
